8f291c0a7d457cd96716f688cd15cf3e1ead427b
YohLouh <yohlourenco25@gmail.com>
2026-02-26 12:13:08 -0300

    Início com JS

---
+++ inicio/comentarios.js
@@ -0,0 +1,12 @@
+
+// Console é utilizado para imprimir algo na tela!
+
+console.log("Sesi");
+console.log("Senai");
+
+/* 
+    Qualquer coisa
+    digitada entre estes
+    não será executado!
+    
+*/
---
+++ inicio/desafio_bloco.js
@@ -0,0 +1,16 @@
+/*
+    Desafio: Criar dois blocos de código.
+    Primeiro imprime na tela os números pares de 0 a 10.
+    Segundo imprime na tela os ímpares.
+*/
+
+{
+    console.log("Números pares:");
+    console.log("0\n2\n4\n6\n8\n10\n");
+}
+
+
+{
+    console.log("Números ímpares:");
+    console.log("1\n3\n5\n7\n9")
+}
