a5c8905e71cded64e8d35e16508d40ce575f163c
YohLouh <yohlourenco25@gmail.com>
2026-03-13 12:18:44 -0300

    Array no JS

---
+++ array/array1.js
@@ -0,0 +1,12 @@
+// ìndices:         0          1          2          3
+const frutas = ["Abacaxi", "Laranja", "Banana", "Melancia"];
+console.log(frutas[0]);
+console.log(frutas[1]);
+console.log(frutas[2]);
+console.log(frutas[3]);
+console.log(frutas);
+console.log(frutas.length);
+
+frutas[4] = "Morango";
+console.log(frutas);
+// ^ caso no lugar de 4 seja 3, substitui o último
---
+++ array/array2.js
@@ -0,0 +1,10 @@
+const nomes = [];
+
+// Desafio: Inserir 5 novos índices com 5 nomes e imprimir na tela
+
+nomes[0] = "Yohanna";
+nomes[1] = "Gleisy";
+nomes[2] = "Diogo";
+nomes[3] = "Neli";
+nomes[4] = "Zuri";
+console.log(nomes);
