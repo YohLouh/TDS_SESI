33aa3271fc221703a0f34eb5ac1fb5980a048f9c
YohLouh <yohlourenco25@gmail.com>
2026-03-20 11:33:17 -0300

    Funções em JS

---
+++ funcoes/funcao_return.js
@@ -0,0 +1,10 @@
+function calcularTotal(preco1, preco2){
+    return preco1 + preco2;
+}
+
+let resultado = calcularTotal(15, 89);
+
+// Desafio: Imprimir concatenando e com template string
+
+console.log(`O preço a pagar será: ${resultado} reais.`);
+console.log("O preço a pagar será: " + resultado + " reais.");
