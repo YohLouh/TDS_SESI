5dfd97b2d45d48a6942d33a44f387ce2c3d75698
YohLouh <yohlourenco25@gmail.com>
2026-03-26 12:10:56 -0300

    Desafio 3

---
+++ atividades/desafio3.js
@@ -0,0 +1,8 @@
+let idade = 19;
+let possuiConvite = true;
+
+if (idade >= 18 && possuiConvite == true){
+    console.log("Você está autorizado a entrar!");
+} else {
+    console.log("Você NÃO está autorizado a entrar.");
+}
